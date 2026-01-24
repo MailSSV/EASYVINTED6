7954697c8e8c828c3478a7d2944d78669208fce2
MailSSV <monadressemailssv1@gmail.com>
2026-01-24 19:11:42 +0100

    Updated AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -60,19 +60,19 @@ export function AppLayout({ children }: AppLayoutProps) {
       setTimeout(() => {
         setShowUserMenu(false);
         setClosingUserMenu(false);
-      }, 250);
+      }, 450);
     } else if (menuType === 'admin') {
       setClosingAdminMenu(true);
       setTimeout(() => {
         setShowAdminMenu(false);
         setClosingAdminMenu(false);
-      }, 250);
+      }, 450);
     } else if (menuType === 'seller') {
       setClosingSellerMenu(true);
       setTimeout(() => {
         setShowSellerMenu(false);
         setClosingSellerMenu(false);
-      }, 250);
+      }, 450);
     }
   };
 
